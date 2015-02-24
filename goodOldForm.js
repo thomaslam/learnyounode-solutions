bfca308198e23437cc88cef4e31742d671685bb3
thomaslam <tuananhlam95@gmail.com>
2015-02-24 10:17:09 -0500

    started expressworks workshop

---
+++ goodOldForm.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+
+
+app.listen(parseInt(process.argv[2]));
--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD");
+var express = require('express');
+var app = express();
+
+app.get('/home', function(req, res) {
+	res.end('Hello World!');
+});
+
+app.listen(parseInt(process.argv[2]));
